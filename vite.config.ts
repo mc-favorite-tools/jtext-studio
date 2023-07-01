f05ac8c87e8c3108d7467f7ae6938086a82fc097
hans000 <hhance@163.com>
2023-07-01 14:35:42 +0800

    feat: modify config

--- vite.config.ts
+++ vite.config.ts
@@ -16,30 +16,30 @@ export default defineConfig({
     },
     plugins: [
         reactRefresh(),
-        cdnImport({
-            modules: [
-                {
-                    name: 'react',
-                    var: 'React',
-                    path: 'https://unpkg.com/react@17.0.2/umd/react.production.min.js',
-                },
-                {
-                    name: 'react-dom',
-                    var: 'ReactDOM',
-                    path: 'https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js',
-                },
-                // {
-                //     name: '@ant-design/icons',
-                //     var: 'icons',
-                //     path: 'https://unpkg.com/@ant-design/icons@4.7.0/dist/index.umd.js',
-                // },
-                {
-                    name: 'antd',
-                    var: 'antd',
-                    path: 'https://unpkg.com/antd@4.21.0/dist/antd.min.js',
-                    css: 'https://unpkg.com/antd@4.21.0/dist/antd.min.css',
-                },
-            ]
-        }),
+        // cdnImport({
+        //     modules: [
+        //         {
+        //             name: 'react',
+        //             var: 'React',
+        //             path: 'https://unpkg.com/react@17.0.2/umd/react.production.min.js',
+        //         },
+        //         {
+        //             name: 'react-dom',
+        //             var: 'ReactDOM',
+        //             path: 'https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js',
+        //         },
+        //         {
+        //             name: '@ant-design/icons',
+        //             var: 'icons',
+        //             path: 'https://unpkg.com/@ant-design/icons@4.7.0/dist/index.umd.js',
+        //         },
+        //         {
+        //             name: 'antd',
+        //             var: 'antd',
+        //             path: 'https://unpkg.com/antd@4.21.0/dist/antd.min.js',
+        //             css: 'https://unpkg.com/antd@4.21.0/dist/antd.min.css',
+        //         },
+        //     ]
+        // }),
     ]
 })
